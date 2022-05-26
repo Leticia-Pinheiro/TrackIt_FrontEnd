c0cdc111bde1e9c31479143c084daa6ad5132f29
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-05-26 02:56:42 -0300

    Tela Cadastro

--- src/componentes/TelaCadastro.js
+++ src/componentes/TelaCadastro.js
@@ -1,5 +1,130 @@
+import React from 'react'
+import styled from "styled-components"
+import axios from 'axios'
+import  { Link , useNavigate}  from  'react-router-dom' ;
+
+import  {  useState ,  useEffect  }  from  "react" ;
+
+
 export default function TelaCadastro(){
-    return(
-        <></>
+
+    let navigate = useNavigate();
+    
+    const [cadastro, setCadastro] = React.useState({
+        email: '',
+        name: '',
+        image: '',
+        password: '',
+    })
+
+    function FazerCadastro(event){
+        event.preventDefault();        
+    }
+
+    function TrocaCadastro(e){
+        setCadastro({
+            ...cadastro,
+            [e.target.name]: e.target.value,
+          }) 
+    }
+
+    function Limpar(){
+        setCadastro({
+        email: '',
+        name: '',
+        image: '',
+        password: '',
+        })
+    }
+    
+    function Cadastrar(){
+        const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", cadastro)
+        
+        promisse.then(res => {
+            console.log(res.data)
+            navigate("/");
+        })
+
+        promisse.catch(erro => {
+            console.log(erro)
+            alert("Usuário já cadastrado!")
+            Limpar()
+        })
+        
+    }
+
+    return( 
+        <form onSubmit={FazerCadastro}>
+        <Container>
+
+            <Foto src = "imagens/TrackIt.png" alt = "logo"/>            
+            <CaixaDeTexto name="email" type="email" placeholder="email" value = {cadastro.email} onChange={TrocaCadastro} required  />
+            <CaixaDeTexto name="password" type="password" placeholder="senha" value = {cadastro.password} onChange={TrocaCadastro} required />
+            <CaixaDeTexto name="name" type="text" placeholder="nome" value = {cadastro.name} onChange={TrocaCadastro} required  />
+            <CaixaDeTexto name="image" type="url" placeholder="foto" value = {cadastro.image} onChange={TrocaCadastro} required  />
+            <BotaoCadastrar onClick={Cadastrar}>Cadastrar</BotaoCadastrar>
+                        
+            <Link to = '/'>
+                <LinkLogin>Já tem uma conta? Faça login!</LinkLogin>
+            </Link>
+            
+        </Container>       
+        </form> 
     )
 }
+
+
+    const Container = styled.div `
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;`    
+
+    const Foto = styled.img `
+        margin: 60px 0;
+        height: 180px;
+        width: auto;`    
+
+    const CaixaDeTexto = styled.input `
+        margin: 3px;
+        box-sizing: border-box;    
+        width: 303px;
+        height: 45px;
+        background: #FFFFFF;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        
+        ::placeholder{
+            color:#DBDBDB;
+        }`
+
+    const BotaoCadastrar = styled.button `
+        margin-top:3px;
+        margin-bottom:25px;
+        width: 303px;
+        height: 45px;
+        background: #52B6FF;
+        border-radius: 5px;
+        font-family:'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 21px;
+        line-height: 26px;
+        text-align: center;
+        color: #FFFFFF;`    
+
+    const LinkLogin = styled.span `        
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+        color: #52B6FF;`
--- src/componentes/TelaLogin.js
+++ src/componentes/TelaLogin.js
@@ -1,43 +1,83 @@
 import React from 'react'
 import styled from "styled-components"
+import axios from 'axios'
+import  {Link , useNavigate}  from  'react-router-dom' ;
+import  {  useState ,  useEffect  }  from  "react" ;
 
 
 export default function TelaLogin(){
-    return( 
-        <Container>
-            <Foto src = "imagens/TrackIt.png" alt = "logo"/>
-            <CaixaDeTexto type="text" placeholder="email" />
-            <CaixaDeTexto type="text" placeholder="senha" />
-            <Entrar>Entrar</Entrar>
-            <span>Não tem uma conta? Cadastre-se!</span>
-        </Container>       
+    
+    const navigate = useNavigate()
+    
+    const [login, setLogin] = React.useState({
+        email: '',
+        password: '',
+    })
+
+    function FazerLogin(event){
+        event.preventDefault();
+        console.log(login)
+    }
+
+    function TrocaLogin(e){
+        setLogin({
+            ...login,
+            [e.target.name]: e.target.value,
+          }) 
+    }
+    
+    function Limpar(){
+        setLogin({
+            email: '',
+            password: ''
+        })
+    }
+    
+    function Logar(){
+        const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", login)
         
+        promisse.then(res => {
+            console.log(res.data)
+            navigate("/hoje");
+        })
+
+        promisse.catch(erro => {
+            console.log(erro)
+            alert("Email ou senha incorretos!")
+            Limpar()
+        })
+    }
+
+    return( 
+        <form onSubmit={FazerLogin}>
+        <Container>
+
+            <Foto src = "imagens/TrackIt.png" alt = "logo"/>            
+            <CaixaDeTexto name="email" type="email" placeholder="email" value = {login.email} onChange={TrocaLogin}  required  />
+            <CaixaDeTexto name="password" type="password" placeholder="senha" value = {login.password} onChange={TrocaLogin} required />
+            <Entrar onClick={Logar}>Entrar</Entrar>
+                        
+            <Link to = '/cadastro'>
+                <LinkCadastro>Não tem uma conta? Cadastre-se!</LinkCadastro>
+            </Link>
+            
+        </Container>       
+        </form> 
     )
 }
 
-const Container = styled.div `
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center
-    
-    input {    
-        
-    }
-    
-    span{
-        margin-top:22px;
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 13.976px;
-        line-height: 17px;
-        text-align: center;
-        text-decoration-line: underline;
 
-        color: #52B6FF;
-    }`
+    const Container = styled.div `
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;`    
+
+    const Foto = styled.img `
+        margin: 60px 0;
+        height: 180px;
+        width: auto;`    
 
     const CaixaDeTexto = styled.input `
         margin: 3px;
@@ -52,11 +92,14 @@ const Container = styled.div `
         font-weight: 400;
         font-size: 19.976px;
         line-height: 25px;
-        color: #DBDBDB;
         
-    `
+        ::placeholder{
+            color:#DBDBDB;
+        }`
+
     const Entrar = styled.button `
-        margin:3px;
+        margin-top:3px;
+        margin-bottom:25px;
         width: 303px;
         height: 45px;
         background: #52B6FF;
@@ -67,9 +110,14 @@ const Container = styled.div `
         font-size: 21px;
         line-height: 26px;
         text-align: center;
-        color: #FFFFFF;`
+        color: #FFFFFF;`    
 
-    const Foto = styled.img `
-        margin: 60px 0;
-        height: 180px;
-        width: auto;`
+    const LinkCadastro = styled.span `        
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+        color: #52B6FF;`
