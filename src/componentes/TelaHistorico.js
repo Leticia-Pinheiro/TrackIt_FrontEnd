0cbbda88dbbd9369fb441636d879c3cf93ab01a6
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-05-29 16:44:43 -0300

    Histórico

--- src/componentes/TelaHistorico.js
+++ src/componentes/TelaHistorico.js
@@ -1,32 +1,37 @@
-import { useEffect, useContext} from "react"
-import axios from 'axios'
+// import { useEffect, useContext} from "react"
+// import axios from 'axios'
 import styled from 'styled-components'
-import UserContext from "./contexts/UserContext"
+// import UserContext from "./contexts/UserContext"
 import TopoMenu from "./TopoMenu"
 
 
 export default function TelaHistorico(){
     
-    const {dados} = useContext(UserContext)    
-    const token = dados.token
+    // const {dados} = useContext(UserContext)    
+    // const token = dados.token
 
-    useEffect(() => {
-        const config = {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        }
+    // useEffect(() => {
+    //     const config = {
+    //         headers: {
+    //             "Authorization": `Bearer ${token}`
+    //         }
+    //     }
 
-        const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today", config)
-        promise.then(console.log(token))
-    })
+    //     const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today", config)
+    //     promise.then(console.log(token))
+    // })
     
 
     return(
         <Container>
             <TopoMenu/>
+            <Titulo>
+                <span>Histórico</span>                  
+            </Titulo>                        
             
-            <h1>TELA Historico</h1>
+            <Mensagem>Em breve você poderá ver o histórico dos seus hábitos aqui!</Mensagem>
+            
+            
         </Container>
         
         
@@ -34,7 +39,40 @@ export default function TelaHistorico(){
     )
 }
 
-const Container = styled.div `
+const Container = styled.div `    
     margin-top: 70px;
     height: 100vh;
-    background-color: #E5E5E5;`
+    background-color: #E5E5E5;
+    display:flex;
+    flex-direction: column;    
+    align-items:center;`
+    
+
+const Titulo = styled.div `
+    width: 340px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding:28px 18px;
+    
+    span{
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 23px;
+        line-height: 29px;
+        color: #126BA5;
+    }`
+    
+    
+
+const Mensagem = styled.div `
+    margin-top:28px;
+    padding: 0 18px;    
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #666666;
+    `
